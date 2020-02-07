39d749548558043f50284b016e750f5456f95ad1
finncallahan123 <finnc129@gmail.com>
2020-02-07 10:30:34 -0500

    Add app.py

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index.html')
+@app.route('/index')
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-
+@app.route('/cart')
+def home():
+    return render_template('cart.html', cart=cart)
 
 if __name__ == '__name__':
-    app.run(debug=true)
+    app.run(debug=True)
